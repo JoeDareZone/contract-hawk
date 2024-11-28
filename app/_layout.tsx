7412af8dde1542dc52e29fd978f5c54bcb4816b4
Joe Ulyatt <joewhocodes@gmail.com>
2024-11-28 10:48:50 +0100

    fix: :bug: Include components in tailwind config file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,38 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { useFonts } from 'expo-font';
-import { Slot } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { useFonts } from 'expo-font'
+import { Slot } from 'expo-router'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import 'react-native-reanimated'
 
-import '../global.css';
+import '../global.css'
 
-export { ErrorBoundary } from 'expo-router';
+export { ErrorBoundary } from 'expo-router'
 
 export const unstable_settings = {
 	// Ensure that reloading on `/modal` keeps a back button present.
 	initialRouteName: '(tabs)',
-};
+}
 
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
 	const [loaded, error] = useFonts({
 		SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
 		...FontAwesome.font,
-	});
+	})
 
 	useEffect(() => {
-		if (error) throw error;
-	}, [error]);
+		if (error) throw error
+	}, [error])
 
 	useEffect(() => {
-		loaded && SplashScreen.hideAsync();
-	}, [loaded]);
+		loaded && SplashScreen.hideAsync()
+	}, [loaded])
 
-	!loaded && null;
+	!loaded && null
 
-	return <Slot />;
+	return <Slot />
 }
 
 // function RootLayoutNav() {
--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,27 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
 
 export default function EditScreenInfo({ path }: { path: string }) {
-  return (
+	return (
 		<View>
-			<View style={styles.getStartedContainer}>
-				<Text style={styles.getStartedText}>
-					Upload a file to get started
-				</Text>
+			<View>
+				<Text className='text-lg'>Upload a file to get started</Text>
 			</View>
 		</View>
-  );
+	)
 }
 
 const styles = StyleSheet.create({
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  getStartedText: {
-    fontSize: 17,
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-});
+	getStartedContainer: {
+		alignItems: 'center',
+		marginHorizontal: 50,
+	},
+	getStartedText: {
+		fontSize: 17,
+		lineHeight: 24,
+		textAlign: 'center',
+	},
+})
