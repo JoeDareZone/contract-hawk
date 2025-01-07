85b386b57e36e800c4b5082a76e577b91f328b6d
Joe Ulyatt <joewhocodes@gmail.com>
2025-01-07 13:20:17 +0100

    feat: :sparkles: Add pdf extractor package and component

--- components/UploadFile.tsx
+++ components/UploadFile.tsx
@@ -3,9 +3,16 @@ import { Pressable, Text, View } from 'react-native'
 import DocumentPicker, {
 	DocumentPickerResponse,
 } from 'react-native-document-picker'
+import { Extractor, Patterns } from 'react-native-pdf-extractor'
+import { Transient } from 'react-native-pdf-extractor/src/types'
 
 export default function UploadFile() {
 	const [file, setFile] = useState<DocumentPickerResponse>()
+	const [pages, setPages] = useState<number>()
+	const [isEncrypted, setIsEncrypted] = useState<boolean>()
+	const [uri, setUri] = useState<string>()
+	const [time, setTime] = useState<string>()
+	const [result, setResult] = useState<string[]>()
 
 	const pickDocument = async () => {
 		const pickedFile = await DocumentPicker.pickSingle({
@@ -13,6 +20,14 @@ export default function UploadFile() {
 		})
 		setFile(pickedFile)
 	}
+
+	const onResult = (data: Transient | null) => {
+		setPages(data?.pages);
+		setIsEncrypted(data?.isEncrypted);
+		setUri(data?.uri);
+		setTime(data?.duration);
+		setResult(data?.text as string[]);
+	  };
 
 	useEffect(() => {
 		console.log('file was picked succesfully', file)
@@ -25,6 +40,17 @@ export default function UploadFile() {
 				onPress={pickDocument}
 			>
 				<Text className='text-lg text-white'>Upload</Text>
+			</Pressable>
+			<Pressable
+				className='border-2 py-2 px-4 mb-6 rounded-lg bg-slate-600 active:bg-blue-600'
+				// onPress={callback}
+			>
+				<Extractor
+					onResult={onResult}
+					patterns={Patterns.Common.Email}
+					uri={file?.uri}
+					title='hi'
+				/>
 			</Pressable>
 			<Text className='text-md text-black'>{file?.name}</Text>
 		</View>
